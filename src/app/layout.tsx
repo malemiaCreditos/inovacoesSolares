27ab0fe838800a2918d9368217746d864557ee5f
dvmicrocredito <dvmicrocredito@gmail.com>
2024-10-18 11:07:16 +0200

    responsividade bg white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-white">
       <body className={inter.className}>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
